5975a929ed2d9674cd8c2f70efdd14efb1a1a019
Ormendel <orimendel@gmail.com>
2024-03-21 13:54:06 +0200

    Creating Player class (INCOMPLETE)

---
+++ Player.rb
@@ -0,0 +1,22 @@
+class Player
+    @@no_players = 0
+    def initialize(name)
+       @name = name
+       @letter = name[0].upcase()
+       @@no_players += 1
+    end
+    def getName()
+        return @name
+     end
+     def getLetter()
+        return @letter
+     end
+     def self.variable
+        return @@no_players # Return the value of this variable
+     end
+ end
+
+player1 = Player.new('or') # It calls initialize function
+player2 = Player.new('tom')
+
+puts "total players playing: #{Player.variable}"
